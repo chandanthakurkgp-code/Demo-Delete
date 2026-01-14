b2115ea83fd858baeda6713fa2f3f3a31e4c14b1
chandanthakurkgp-code <chandan.thakurkgp@gmail.com>
2026-01-14 14:48:42 +0530

    Add files via upload

--- saloonData.js
+++ saloonData.js
@@ -1,13 +1,10 @@
-﻿const saloonData = {
+const saloonData = {
     "saloon": {
         "title": "Saloon",
         "icon": "✂️",
-        "rates": ["Haircut: ₹100", "Shave: ₹60", "Facial: ₹500", " For home visit per person (Monday, Tuesday, Thursday, Saturday) extra charge is ₹100"],
+        "rates": ["Haircut: ₹100", "Shave: ₹60", "Facial: ₹500"],
         "partners": [
-            { "name": "आर्यन हेयर सैलून", "address": "गोल बाज़ार", "mobile": "9800012311", "whatsapp": "919800012311", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
-            { "name": "Ram1", "address": "गोल बाज़ार", "mobile": "9800012321", "whatsapp": "919800012321", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
-            { "name": "Ram2", "address": "गोल बाज़ार", "mobile": "9800012331", "whatsapp": "919800012331", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
-            { "name": "Ram3", "address": "गोल बाज़ार", "mobile": "9800012341", "whatsapp": "919800012341", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
+            { "name": "आर्यन हेयर सैलून", "address": "गोल बाज़ार", "mobile": "9800012301", "whatsapp": "919800012301", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
             { "name": "रॉयल कट सैलून", "address": "गेट बाज़ार", "mobile": "9800012302", "whatsapp": "919800012302", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz2" }
         ]
     }
