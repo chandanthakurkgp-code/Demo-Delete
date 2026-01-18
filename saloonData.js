7c8758e3317cf36416c489725c34c5503651c046
chandanthakurkgp-code <chandan.thakurkgp@gmail.com>
2026-01-19 01:02:41 +0530

    Update print statement to say 'Goodbye World'

--- saloonData.js
+++ saloonData.js
@@ -2,7 +2,7 @@
     "saloon": {
         "title": "Saloon",
         "icon": "✂️",
-        "rates": ["Haircut: ₹100", "Shave: ₹60", "Facial: ₹500", " For home visit per person (Monday, Tuesday, Thursday, Saturday) extra charge is ₹100],
+        "rates": ["Haircut: ₹100", "Shave: ₹60", "Facial: ₹500"],
         "partners": [
             { "name": "आर्यन हेयर सैलून", "address": "गोल बाज़ार", "mobile": "9800012301", "whatsapp": "919800012301", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
             { "name": "आर्यन हेयर सैलून", "address": "गोल बाज़ार", "mobile": "9800012301", "whatsapp": "919800012301", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
@@ -437,4 +437,5 @@
         ]
     }
 };
+
 
